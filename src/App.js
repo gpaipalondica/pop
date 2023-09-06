3f9e80ecfa3f799726856e92a6e1e6b8d1dbe85b
gpaipalondica <gpaipalondica@sycamores.indstate.edu>
2023-09-05 20:25:07 -0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Apple from './components/Apple';
+import Lamborghini from './components/Lamborghini';
+import Starbucks from './components/Starbucks';
+import Pizzaweb from './components/Pizzaweb/Pizzaweb';
+import PizzaAbout from './components/Pizzaweb/PizzaAbout';
+import PizzaMenu from './components/Pizzaweb/PizzaMenu';
+import PizzaCart from './components/Pizzaweb/PizzaCart';
+import { ShopContextProvider } from './components/Pizzaweb/helpers/PizzaShopContext';
+import PizzaCheckout from './components/Pizzaweb/PizzaCheckout';
+import Movies from './components/Movies/Movies';
+import MovieItem from './components/Movies/MovieItem';
+import FacebookMobile from './components/FacebookMobile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShopContextProvider>
+      <Router>
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/appleweb' Component={Apple}></Route>
+          <Route path='/lamborghini' Component={Lamborghini}></Route>
+          <Route path='/starbucks' Component={Starbucks}></Route>
+          <Route path='/pizzaweb' Component={Pizzaweb}></Route>
+          <Route path='/pizzaweb/about' Component={PizzaAbout}></Route>
+          <Route path='/pizzaweb/menu' Component={PizzaMenu}></Route>
+          <Route path='/pizzaweb/cart' Component={PizzaCart}></Route>
+          <Route path='/pizzaweb/checkout' Component={PizzaCheckout}></Route>
+          <Route path='/moviemania' Component={Movies}></Route>
+          <Route path='/moviemania/:title' Component={MovieItem}></Route>
+          <Route path='/facebookmobile' Component={FacebookMobile}></Route>
+        </Routes>
+      </Router>
+      </ShopContextProvider>
     </div>
   );
 }
